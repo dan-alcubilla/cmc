af809d3fbd0e22572f3c9260b99f450224e5ed14
Alexis Daniel <86176019+dan-alcubilla@users.noreply.github.com>
2023-07-04 07:35:48 -0400

    Primeros pasos
    Gestión de historias medicas

---
+++ app/Controllers/Http/PagesController.ts
@@ -0,0 +1,15 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class PagesController {
+    public home({ view }: HttpContextContract) {
+        return view.render('welcome')
+    }
+
+    public historias({ view }: HttpContextContract) {
+        return view.render('historias')
+    }
+
+    public consultas({ view }: HttpContextContract) {
+        return view.render('consultas')
+    }
+}
